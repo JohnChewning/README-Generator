a94b78933c8eefc6345736cc73a6dcf306804a20
JohnChewning <johnathanchewning@gmail.com>
2024-02-01 11:36:10 -0500

    finished addinf functions to index.js

--- develop/index.js
+++ develop/index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require('../utils/generateMarkdown');
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, fileText) {
+ 
+  fs.writeFile(fileName, fileText, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+}
+
+function generateLicense(license) {
+  const licenseBadges = {
+    MIT: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+    "Apache 2.0 License": "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+    "IBM Public License Version 1.0": "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
+    "Mozilla Public License 2.0": "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+    Unlicense: "[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)",
+  };
+
+  return licenseBadges[license] || "";
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+  // TODO: Create an array of questions for user input
+    .prompt([
+      { type: "input", message: "What is your name?", name: "name" },
+      { type: "input", message: "What is the title of your project?", name: "title" },
+      { type: "input", message: "Add the description of your project:", name: "description" },
+      { type: "input", message: "Add the installation instructions of your project:", name: "installation" },
+      { type: "input", message: "Add the usage information of your project:", name: "usage" },
+      { type: "input", message: "Add the contribution guidelines of your project:", name: "contribution" },
+      { type: "input", message: "Add the test instructions of your project:", name: "test" },
+      { type: "list", message: "Select the type of license you would like for your project:", choices: ["MIT", "Apache 2.0 License", "IBM Public License Version 1.0", "Mozilla Public License 2.0", "The Unlicense"], name: "license" },
+      { type: "input", message: "What is your GitHub handle?", name: "github" },
+      { type: "input", message: "What is your email?", name: "email" },
+    ])
+    .then((data) => {
+      writeToFile("sample-README.md", generateMarkdown(data));
+    });
+}
 
 // Function call to initialize app
 init();
+
+
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ''; // No license, return empty string
+  }
+
+  // Map the license to its corresponding badge URL (replace 'LICENSE_TYPE' with the actual license type)
+  const badgeURL = `https://img.shields.io/badge/license-${license}-brightgreen`;
+  return `![License](${badgeURL})`;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return ''; // No license, return empty string
+  }
+
+  // Map the license to its corresponding URL or replace 'LICENSE_TYPE' with the actual license type URL
+  const licenseURL = `https://opensource.org/licenses/${license}`;
+  return `[License](${licenseURL})`;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+    return ''; // No license, return empty string
+  }
+
+  // You can customize this section with additional details or just use the badge and link
+  return `
+## License
+
+This project is licensed under the [${license} license](https://opensource.org/licenses/${license}).
+`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseLink = renderLicenseLink(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
+${licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${licenseLink}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+For any questions, please contact ${data.email}.
+${licenseSection}
 `;
 }
 
